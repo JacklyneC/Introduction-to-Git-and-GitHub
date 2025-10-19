f4e1122632767fc1fdd51597423ea9ac3686e190
maplerocks <39458101+maplerocks@users.noreply.github.com>
2025-10-19 16:17:41 +0800

    Update new.py

--- new.py
+++ new.py
@@ -1,2 +1,2 @@
 print("Hello, world.")
-print("Welcome To Git and GitHub Introduction - Using Fork and Pull Contributed by MapleRock")
+print("Welcome To Git and GitHub Introduction - Using Fork and Pull Contributed by MapleRock!")
